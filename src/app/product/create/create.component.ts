dfb4a18d1056bc6dc302a1b8ea9904367e589ec8
vu <duongtanvu096@gmail.com>
2021-11-14 21:43:07 +0700

    _add: get datasource type combobox

--- src/app/product/create/create.component.ts
+++ src/app/product/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploadControl, FileUploadValidators } from '@iplab/ngx-file-upload';
+import { TypeService } from "../../services/type.service";
+import { SelectionModel } from "../../models/selection.model";
 
 @Component({
   selector: 'app-create',
@@ -7,8 +9,18 @@ import { FileUploadControl, FileUploadValidators } from '@iplab/ngx-file-upload'
   styleUrls: ['./create.component.less']
 })
 export class CreateComponent implements OnInit {
-  public fileUploadControl = new FileUploadControl({ listVisible: false });
-  constructor() { }
+  fileUploadControl = new FileUploadControl({ listVisible: false });
+  types: SelectionModel[] = [];
+  constructor(private typeService: TypeService) { 
+    this.typeService.getTypes().subscribe(results => {
+      this.types = results.map((data) =>{
+        return <SelectionModel>{
+          value: data.id,
+          name: data.name
+        }
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
